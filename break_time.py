6396c583454729e5e361e34754573c51ff497541
mikeyhavoc <m.jwilliams@outlook.com>
2016-07-31 17:01:09 -0400

    add comments, imports, and webbrowser to open up to duality

---
+++ break_time.py
@@ -0,0 +1,9 @@
+# break_time
+# michael Williams
+# 0.5
+# 7/31/2016
+# timer to take a break from my computer to go outside and vape.
+import time
+import webbrowser
+
+webbrowser.open('https://www.youtube.com/watch?v=i-Q2oTRLrK8')
