06fa94fc0f01eccac783f80d4a99b293aedf800d
mikeyhavoc <m.jwilliams@outlook.com>
2016-07-31 17:34:20 -0400

    put in def break_time, var breaks for how many breaks you would like to set, timers still in place, set for every 5 minutes.

--- break_time.py
+++ break_time.py
@@ -6,4 +6,19 @@
 import time
 import webbrowser
 
-webbrowser.open('https://www.youtube.com/watch?v=i-Q2oTRLrK8')
+# asks how many breaks to do for a day
+breaks = input('how many breaks would you like? ')
+
+def break_time(breaks):
+
+    time_for_break = 0
+    while(time_for_break < int(breaks)):
+        time.sleep( (60 * 1) * 5)
+        timer = time.ctime()
+        print(timer)
+        webbrowser.open('https://www.youtube.com/watch?v=i-Q2oTRLrK8')
+        time_for_break = time_for_break + 1
+
+break_time(breaks)
+times = time.ctime()
+print(times)
